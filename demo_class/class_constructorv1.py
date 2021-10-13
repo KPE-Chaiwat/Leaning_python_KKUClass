ee674c85b80a43816d6c0412aa0cf43040c305b0
Good Boy <Lawliet10478@gmail.com>
2021-10-13 16:02:07 +0700

    ff

---
+++ demo_class/class_constructorv1.py
@@ -0,0 +1,13 @@
+class Examples:
+    def __init__(self):
+        print("I am the first Constructor")
+    def __init__(self):
+        print("I am the second Constructor")
+    def __init__(self):
+        print("I am the third Constructor")
+    def __init__(self):
+        print("I am the fourth Constructor")
+        print("Object always call the last")
+        print("Constructor . that's why I'm here")
+
+obj = Examples()

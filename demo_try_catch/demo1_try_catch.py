6ff48fa56fba1cb09d3300f05f156dbecef42626
Good Boy <Lawliet10478@gmail.com>
2021-10-13 22:35:17 +0700

    ff

---
+++ demo_try_catch/demo1_try_catch.py
@@ -0,0 +1,32 @@
+import math
+
+class Cartesian:
+       def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+       def distance(self):
+        dimension = self.x**2 + self.y**2 + self.z**2
+        radius = math.sqrt(dimension)
+        print("ระยะทางจากจุด (0,0,0) ไปยังจุด (",self.x,",",self.y,",",self.z,")คืิอ",radius,"หน่วย")
+
+if __name__ == "__main__":
+ while True:
+    try:
+       print("กรอกตัวเลขจุด x,y,z ")
+       axis_x = int(input("ค่าแกนx:"))
+       axis_y = int(input("ค่าแกนy:"))
+       axis_z = int(input("ค่าแกนz:"))
+       Obj = Cartesian(axis_x, axis_y, axis_z)
+       Obj.distance()
+       break
+    except ValueError: 
+       print("ใส่ตัวเลขซิครับ ปัดโถ่(เสียงลุงแถวบ้าน)")
+    except :
+       print("Something Wrong")
+    
+
+ 
+
+
+   
